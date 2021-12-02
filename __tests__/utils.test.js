88db9043730d687076ad55c88beab4f9f539895b
ed <edheyev@gmail.com>
2021-12-02 12:33:01 +0000

    correct utils test

--- __tests__/utils.test.js
+++ __tests__/utils.test.js
@@ -3,7 +3,7 @@ const { removeApostrophe } = require("../utils/utils");
 describe("removeApostrope", () => {
   test("should remove apostrophe and replace with '", () => {
     const input = "Ed's Input";
-    const expected = "Ed\\'s Input";
+    const expected = "Ed''s Input";
     const actual = removeApostrophe(input);
     expect(actual).toBe(expected);
   });
